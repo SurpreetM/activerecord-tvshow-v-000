a6d88274b105a77a719150278006717b18202766
surpreet.mundra <surpreet.mundra@gmail.com>
2018-10-19 18:08:08 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,11 +9,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.lowest_rating
-    Show.order(rating: :asc).first
+    Show.order(rating: :asc).first.rating
   end
 
   def self.least_popular_show
-    Show.order(rating: :asc).first
+    Show.order(rating: :asc).first.
   end
 
 end
